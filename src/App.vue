e361e3e1f3f671e13de7f13ee54fb7dec6e419eb
Myunghun Park <mh97888@gmail.com>
2019-01-24 14:40:07 +0900

    Design ver 2.0
    Calender추가 버전입니다.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <!-- Vue 템플릿(HTML) -->
 <template>
-    <div>
+    <div class="container">
+        <div class="container_left"> 
+            <div class="calender"></div>
+            <div class="container_space"></div>
+            <div class="toDolist"></div>
+            <div class="container_space"></div>
+            <div class="memo"></div>
+        </div>
+        <div class="container_right"> 
+            <div class="search_setting">
+                <div class="search_space"></div>
+                <select class="search_select" name="brouser">
+                    <option value="google">Google</option>
+                    <option value="naver">Naver</option>
+                    <option value="daum">Daum</option>
+                </select>
+            </div>
 
+            <div class="search_page"></div>
+        </div>
     </div>
 </template>
 
@@ -22,6 +40,50 @@ export default {
 
 <!-- CSS -->
 <style lang = "scss">
+body{
+    margin:0;
+}
+
+.container{
+    height: 800px;
+    display: flex;
+}
+
+.container_left{
+    width: 300px;
+    height: 100%;
+    border: 1px solid white;
+    background: #FFEEE4;
+    display: inline-block;
+}
+.calender{}
+.container_space{}
+.toDolist{}
+.container_space{}
+.memo{}
+
+.container_right{
+    flex: 1;
+    height: 100%;
+    background: white;
+    display: inline-block;
+} 
+
+.search_setting{
+
+}
+
+.search_space{
+
+}
+
+.search_select{
+
+}
+
+.search_page{
+
+}
 
 </style>
 
